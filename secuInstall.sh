a3f94098f402a443ea98684f2ba8bd8a892ae89a
root <root@localhost.localdomain>
2019-05-15 10:52:31 +0200

    tools secu

---
+++ secuInstall.sh
@@ -0,0 +1,19 @@
+cd /opt/
+git clone https://github.com/samratashok/nishang
+mkdir AD
+cd AD
+git clone https://github.com/BloodHoundAD/BloodHound
+git clone https://github.com/SecureAuthCorp/impacket
+git clone https://github.com/ropnop/impacket_static_binaries
+git clone https://github.com/gentilkiwi/mimikatz
+git clone https://github.com/ropnop/kerbrute
+git clone https://github.com/HarmJ0y/ASREPRoast
+git clone https://github.com/GhostPack/Rubeus
+git clone https://github.com/gentilkiwi/kekeo.git
+cd ..
+mkdir reseau
+cd reaseau
+git clone https://github.com/klsecservices/rpivot
+git clone https://github.com/byt3bl33d3r/CrackMapExec
+
+sudo apt-get install sshuttle
